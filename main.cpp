fcc5ede1c2fbbc5b781e8beece280e311c364f77
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2020-09-29 09:35:38 -0500

    prueb101

--- main.cpp
+++ main.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    unsigned int base, altura;
+
+    cin >> base;
+    cin >> altura;
+
+    cout << (base * altura) / 2 << endl;
 
     return 0;
 }
